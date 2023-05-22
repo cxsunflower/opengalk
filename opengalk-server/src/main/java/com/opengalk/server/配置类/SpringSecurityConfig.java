b067b55687a8b24515a2dfbcfcf698ac139f2ea6
向日葵 <1527981693@qq.com>
2023-05-22 09:51:06 +0800

    更新了spring security配置

--- opengalk-server/src/main/java/com/opengalk/server/配置类/SpringSecurityConfig.java
+++ opengalk-server/src/main/java/com/opengalk/server/配置类/SpringSecurityConfig.java
@@ -7,9 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -63,12 +65,12 @@ public class SpringSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(@NonNull HttpSecurity http) throws Exception {
         return http
-                .cors()
-                .and()
+                // 跨域配置
+                .cors(Customizer.withDefaults())
                 // 不使用csrf防护
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 // 登陆
                 .formLogin(
                         formLoginCustomizer -> formLoginCustomizer
